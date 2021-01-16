fff2e2d372ee74b3d6ed4bb59582750f003507fd
Andrzej Stasinski <andis_as@wp.pl>
2021-01-16 21:48:55 +0100

    new component ToDoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import './App.css'
 
-export default function App() {
+class ToDoList extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <h2>{this.props.title}</h2>
+    )
+  }
+}
+
+const App = () => {
 
     return (
-      <div>
         <div className="App">
-          <h2>App</h2>
+          <ToDoList title='My ToDo List' />
         </div>
-      </div>
     );
 }
 
+export default App
