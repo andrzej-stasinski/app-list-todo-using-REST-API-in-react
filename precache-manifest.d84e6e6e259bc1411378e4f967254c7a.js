f079cac516f3a8e1d17256a325c8661120fcd1f3
Andrzej Stasinski <andis_as@wp.pl>
2021-01-22 18:50:41 +0100

    Updates

---
+++ precache-manifest.d84e6e6e259bc1411378e4f967254c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac6c8a7f3f82fb5826719ef7a6255de",
+    "url": "/app-list-todo-using-REST-API-in-react/index.html"
+  },
+  {
+    "revision": "8fc7ef024b2380754dd3",
+    "url": "/app-list-todo-using-REST-API-in-react/static/css/main.b11fa65a.chunk.css"
+  },
+  {
+    "revision": "616202f2be69a8ff85c1",
+    "url": "/app-list-todo-using-REST-API-in-react/static/js/2.6de27c9a.chunk.js"
+  },
+  {
+    "revision": "8fc7ef024b2380754dd3",
+    "url": "/app-list-todo-using-REST-API-in-react/static/js/main.743b832c.chunk.js"
+  },
+  {
+    "revision": "7c8b08ffb141b0308218",
+    "url": "/app-list-todo-using-REST-API-in-react/static/js/runtime-main.316bb2d9.js"
+  }
+]);
